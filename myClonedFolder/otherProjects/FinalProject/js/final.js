fe39333798ab8c1c1a1df8e161153003cfe94a4d
pinkbun <buiquan478@gmail.com>
2016-04-09 21:01:31 -0400

    change this shit up

--- myClonedFolder/otherProjects/FinalProject/js/final.js
+++ myClonedFolder/otherProjects/FinalProject/js/final.js
@@ -1,129 +1,123 @@
-$(document).ready(function(){
-	
-	// Get all the nav li, add click event
-	$(".nav").find("li").on("click",function(){
-		// remove all active class
-		$(".nav").find("li").removeClass("active");
-		// add active class to clicked it. 
-		$(this).addClass("active");
-		var page = $(this).attr("id");
-		console.log("page" +page);
-		getPartial(page);
-	
-	});// closes the $(".nav").find("li").on("click",function()
-	
-	function getPartial(partial) {
-		
-		if (partial === "homePage"){
-				$.get("partials/home.html", function(data){
-					$("#pageContent").html(data);
-					$(".carousel").carousel();
-					
-		});
-		
-		}else if (partial === "secondPage"){
-					$.getJSON("jsonDatabase/final.json",function(data){
-		 console.dir(data);
+$(document).ready(function() {
 
-                    var html = "";
-                    $.each(data, function (index, item) {
-                        html += '<div class="col-md-4 bun">' +
-                            '<div class="bunName">' + item.name + '</div>' +
-                            '<div class="bunType">' + item.type + '</div>' +
-                            '<div class="bunGender">' + item.gender + '</div>' +
-                            '<img class="bunImg" src="' + item.image + '"/>' +
-                            '<div class="commentsContainer">';
-                        $.each(item.comments, function (index, item) {
-                            html += '<div class="renterName">' + item.username + '</div>' +
-                                '<div class="renterComment">' + item.comment + '</div>' +
-                                '<div class="renterStars">';
+  // Get all the nav li, add click event
+  $(".nav").find("li").on("click", function() {
+    // remove all active class
+    $(".nav").find("li").removeClass("active");
+    // add active class to clicked it. 
+    $(this).addClass("active");
+    var page = $(this).attr("id");
+    console.log("page" + page);
+    getPartial(page);
 
-                            var numStars = Number(item.stars)
+  }); // closes the $(".nav").find("li").on("click",function()
 
-                            for (var i = 1; i <= 5; i++) {
-                                if (i <= numStars) {
-                                    html += '<img src="images/starF.png">';
-                                } else {
-                                    html += '<img src="images/starE.png">';
-                                }
-                            }
-                            html += '</div>'; //end stars
-                        }); //each comment
+  function getPartial(partial) {
 
-                        //do some stuff
-                        html += '</div>' + //commentsContainer
-                            '</div>'; //col-md-4
-                    }); //each cat
-                    $("#pageContent").html(html);
-                }); // end 
+    if (partial === "homePage") {
+      $.get("partials/home.html", function(data) {
+        $("#pageContent").html(data);
+        $(".carousel").carousel();
 
-	// ORDER PAGE STARTS
-		}else if (partial === "thirdPage"){
-			
-		$.get("partials/contact.html", function (data) {
-                    $("#pageContent").html(data);
+      });
 
-                    $("#mySingleLineText").on("focus", function () {
-                            $(this).css("background-color", "#F7F8E0");
-                            $("#log").append("<br>input focus");
-                        })
-                        .on("blur", function () {
-                            $("#log").append("<br>input leave");
-                            $(this).css("background-color", "#FFF");
-                        });
+    } else if (partial === "movPage") {
+      $.getJSON("jsonDatabase/final.json", function(data) {
+        console.dir(data);
 
+        var html = "";
+        $.each(data, function(index, item) {
+          html += '<div class="col-md-4 col-xs-8 col-lg-4 col-sm-4 mov">' +
+            '<div class="movName">' + item.name + '</div>' +
+            '<div class="movType">' + item.type + '</div>' +
+            '<img class="movImg" src="' + item.image + '"/>' +
+            '<div class="movSum">' + item.summary + '</div>' +
+            '<div class="commentsContainer">';
+          $.each(item.comments, function(index, item) {
+            html += '<div class="renterName">' + item.username + '</div>' +
+              '<div class="renterComment">' + item.comment + '</div>' +
+              '<div class="renterStars">';
 
-                    $("#mySelect").on("change", function () {
-                        $("#log").append("<br>input change");
-                        var val = $(this).val();
-                        $("#mySelectMessage").html("<br>" + val + " is a nice selection!");
-                    });
+            var numStars = Number(item.stars)
 
-                    $("#myButton").on("mouseenter", function () {
-                            $("#log").append("<br>Button mouseenter");
-                            $(this).text("ORDER NOW!!!");
-                        })
-                        .on("mouseleave", function () {
-                            $("#log").append("<br>Button mouseleave");
-                            $(this).text("Click me!");
-                        });
+            for (var i = 1; i <= 5; i++) {
+              if (i <= numStars) {
+                html += '<img src="images/starF.png">';
+              } else {
+                html += '<img src="images/starE.png">';
+              }
+            }
+            html += '</div>'; //end stars
+          }); //each comment
 
+          //do some stuff
+          html += '</div>' + //commentsContainer
+            '</div>'; //col-md-4
+        }); //each cat
+        $("#pageContent").html(html);
+      }); // end 
 
-                    $("#myButton").on("click", function () {
+      // ORDER PAGE STARTS
+    } else if (partial === "orderPage") {
 
-                        var myInput = $("#mySingleLineText").val();
-                        var myTextarea = $("#myTextarea").val();
-                        var mySelect = $("#mySelect").val();
-                        var myRadio = $("[name='gender']:checked").val();
+        $.get("partials/order.html", function(data) {
+            $("#pageContent").html(data);
 
+            //activate the datepicker
+            $('#startRentDate, #endRentDate').datepicker({});
 
+            //user clicks submit
+            $("#submitButton").on("click", function() {
 
-                        //each is a jquery loop for objects/arrays
-                        //each thing that is selected, do the function
-                        //"this" is the element we are currently Looking at
+              //add the error class to div of empty inputs
+              $("input, select").filter(function() {
+                return !this.value;
+              }).closest("div").addClass("has-error")
 
-                        var allVals = [];
-                        $("[name='vehicle']:checked").each(function () {
-                            allVals.push($(this).val());
-                        });
+              //remove the error class from all filled inputs
+              $("input, select").filter(function() {
+                return this.value;
+              }).closest("div").removeClass("has-error");
 
+              //get all errors
+              var hasError = $(".has-error");
 
-                        $("#log").append("<br>User clicked the button");
-                        $("#log").append("<br>Value of input is: " + myInput);
-                        $("#log").append("<br>Value of textarea is: " + myTextarea);
-                        $("#log").append("<br>Value of select: " + mySelect);
-                        $("#log").append("<br>Value of radio button is " + myRadio);
-                        $("#log").append("<br>Value of check is: " + allVals.join());
+              //if no errors
+              if (hasError.length < 1) {
+                sendConfirmation();
+              }
 
-                        // $("#log").append("<br>");
+            })
 
-                    }); //contact    	 
+          }) //get
+      }
+      $("#pageContent").fadeIn();
 
-		}); 
-			
-			}
-	}
-	// begin Program, get the homepage
-	getPartial("homePage");
-	
-}); //closes document ready. 
+    }
+    //do when order valid
+    function sendConfirmation() {
+
+      //we will store all our order information here
+      var order = {};
+
+      //get all input values
+      var inputs = $("input, select");
+
+      //put all the input values into object ; this each can be done with jquery objects
+      inputs.each(function() {
+        var id = $(this).attr("id");
+        order[id] = $(this).val();
+      })
+
+      //act as if sending to databse
+      alert("send to databse: " + JSON.stringify(order));
+
+      //show success message
+      $("#successMsg").html("Order Received!<br/><br/>" +
+        order.catSelect + " will be delivered on " + order.startRentDate + "<img id='paws' src='images/catPaws.jpeg'>");
+    }//end sendConfirmation
+
+    //begin the program, get the homepage
+    getPartial("homePage");
+
+  }) //ready
